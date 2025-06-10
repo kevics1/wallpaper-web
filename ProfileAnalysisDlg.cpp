ee0473fd2445a72d6cbb7bb221ddd5423f066ddd
kevin <k86359303@gmail.com>
2025-06-10 16:51:48 +0800

    Add files via upload

---
+++ ProfileAnalysisDlg.cpp
@@ -0,0 +1,34 @@
+﻿// ProfileAnalysisDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Terrain.h"
+#include "afxdialogex.h"
+#include "ProfileAnalysisDlg.h"
+
+
+// ProfileAnalysisDlg 对话框
+
+IMPLEMENT_DYNAMIC(ProfileAnalysisDlg, CDialogEx)
+
+ProfileAnalysisDlg::ProfileAnalysisDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_PROFILE_ANALYSIS, pParent)
+{
+
+}
+
+ProfileAnalysisDlg::~ProfileAnalysisDlg()
+{
+}
+
+void ProfileAnalysisDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(ProfileAnalysisDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// ProfileAnalysisDlg 消息处理程序
