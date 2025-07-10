773b00245cd6cf9b0ffd773b1c9185c29e36637c
kevin <k86359303@gmail.com>
2025-07-10 10:16:20 +0800

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,    //添加此项v
-  devServer: {
-    historyApiFallback: true,
-    allowedHosts: "all",
-        }
-})
